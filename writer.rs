e2419a1bb9ecd4bc73c7060c0b4feff5df80ea90
stackinspector <stackinspector@126.com>
2026-01-11 21:33:22 +0800

    value-free-style writer impl

--- writer.rs
+++ writer.rs
@@ -18,7 +18,7 @@ impl<O: Output> Writer<O> {
     }
 
     #[inline(always)]
-    fn bytes<B2: AsRef<[u8]>>(&mut self, bytes: B2) {
+    fn bytes<B: AsRef<[u8]> + ByteStorage>(&mut self, bytes: B) {
         self.output.bytes(bytes);
     }
 
@@ -61,92 +61,183 @@ impl<O: Output> Writer<O> {
         }
     }
 
-    fn uints(&mut self, uints: Box<[u128]>) {
+    fn tag(&mut self, tag: Tag) {
+        self.byte(tag as u8);
+    }
+
+    fn v_uint(&mut self, n: u128) {
+        self.tag(Tag::Uint);
+        self.uleb128(n);
+    }
+
+    fn v_int(&mut self, n: i128) {
+        self.tag(Tag::Int);
+        self.sleb128(n);
+    }
+
+    fn v_uints(&mut self, uints: &[u128]) {
+        self.tag(Tag::Uints);
         for n in uints {
-            self.uleb128(n);
+            self.uleb128(*n);
         }
     }
 
-    fn string(&mut self, usvs: Box<[char]>) {
-        for n in usvs {
-            self.uleb128(n as u128);
+    fn v_bytes<B: AsRef<[u8]> + ByteStorage>(&mut self, bytes: B) {
+        self.tag(Tag::Bytes);
+        self.bytes(bytes);
+    }
+
+    fn v_string(&mut self, chars: &[char]) {
+        self.tag(Tag::String);
+        for char in chars {
+            self.uleb128(*char as u128);
         }
     }
 
-    fn tuple<B: AsRef<[u8]> + ByteStorage>(&mut self, values: Box<[Value<B>]>) {
-        self.uleb128(values.len() as u128);
+    fn h_tuple_need_values(&mut self, len: u128) {
+        self.tag(Tag::Tuple);
+        self.uleb128(len);
+    }
+
+    fn h_tuple_like_need_values(&mut self, tag: Tag, len: u128) {
+        self.tag(tag);
+        self.uleb128(len);
+    }
+
+    fn v_type_id(&mut self, type_id: &TypeId) {
+        self.h_tuple_like_need_values(Tag::TypeId, 2);
+        self.v_uint(type_id.as_type_id_tag() as u8 as u128);
+        match type_id {
+            TypeId::Anonymous => {
+                self.h_tuple_need_values(0);
+            }
+            TypeId::Std { schema, id } => {
+                self.h_tuple_need_values(2);
+                self.v_uint(*schema);
+                self.v_uint(*id);
+            }
+        };
+    }
+
+    fn h_list_need_values(&mut self, r#type: &Type, len: u128) {
+        self.h_tuple_like_need_values(Tag::List, 2);
+        self.v_type(r#type);
+        self.h_tuple_need_values(len);
+    }
+
+    fn v_type(&mut self, r#type: &Type) {
+        self.h_tuple_like_need_values(Tag::Type, 2);
+        self.v_uint(r#type.as_type_tag() as u8 as u128);
+        match r#type {
+            Type::Unknown
+            | Type::Uint
+            | Type::Int
+            | Type::Uints
+            | Type::Bytes
+            | Type::String
+            | Type::Type
+            | Type::TypeId => {
+                self.h_tuple_need_values(0);
+            }
+
+            Type::List(type2) => {
+                self.v_type(type2);
+            }
+
+            Type::Tuple(value_types) => {
+                // if uses list here, h_list and v_type refer to each other. may causes dead loop?
+                self.h_list_need_values(&Type::Type, value_types.len() as u128);
+                for value_type in value_types {
+                    self.v_type(value_type);
+                }
+            }
+
+            Type::Alias(type_id)
+            | Type::Enum(type_id)
+            | Type::Choice(type_id)
+            | Type::Struct(type_id) => {
+                self.v_type_id(type_id);
+            }
+        }
+    }
+
+    fn h_alias_need_value(&mut self, type_id: &TypeId) {
+        self.h_tuple_like_need_values(Tag::Alias, 2);
+        self.v_type_id(type_id);
+    }
+
+    fn v_enum(&mut self, type_id: &TypeId, var_id: VariantId) {
+        self.h_tuple_like_need_values(Tag::Enum, 2);
+        self.v_type_id(type_id);
+        self.v_uint(var_id);
+    }
+
+    fn h_choice_need_value(&mut self, type_id: &TypeId, var_id: VariantId) {
+        self.h_tuple_like_need_values(Tag::Choice, 3);
+        self.v_type_id(type_id);
+        self.v_uint(var_id);
+    }
+
+    fn h_struct_need_values(&mut self, type_id: &TypeId, len: u128) {
+        self.h_tuple_like_need_values(Tag::Struct, 2);
+        self.v_type_id(type_id);
+        self.h_tuple_need_values(len);
+    }
+}
+
+impl<O: Output> Writer<O> {
+    fn v_tuple<B: AsRef<[u8]> + ByteStorage>(&mut self, values: &[Value<B>]) {
+        self.h_tuple_need_values(values.len() as u128);
         for value in values {
             self.value(value);
         }
     }
 
-    fn r#type<B: AsRef<[u8]> + ByteStorage>(&mut self, r#type: Type) {
-        self.value(r#type.encode::<B>());
+    fn v_list<B: AsRef<[u8]> + ByteStorage>(&mut self, r#type: &Type, values: &[Value<B>]) {
+        self.h_list_need_values(r#type, values.len() as u128);
+        for value in values {
+            self.value(value);
+        }
     }
 
-    fn type_id<B: AsRef<[u8]> + ByteStorage>(&mut self, type_id: TypeId) {
-        self.value(type_id.encode::<B>());
+    fn v_alias<B: AsRef<[u8]> + ByteStorage>(&mut self, type_id: &TypeId, value: &Value<B>) {
+        self.h_alias_need_value(type_id);
+        self.value(value);
     }
 
-    fn list<B: AsRef<[u8]> + ByteStorage>(&mut self, r#type: Type, values: Box<[Value<B>]>) {
-        self.value(Value::Tuple(Box::new([
-            Value::Type(r#type),
-            Value::Tuple(values),
-        ])));
+    fn v_choice<B: AsRef<[u8]> + ByteStorage>(&mut self, type_id: &TypeId, var_id: VariantId, value: &Value<B>) {
+        self.h_choice_need_value(type_id, var_id);
+        self.value(value);
     }
 
-    fn alias<B: AsRef<[u8]> + ByteStorage>(&mut self, type_id: TypeId, value: Value<B>) {
-        self.value(Value::Tuple(Box::new([
-            Value::TypeId(type_id),
-            value,
-        ])));
+    fn v_struct<B: AsRef<[u8]> + ByteStorage>(&mut self, type_id: &TypeId, values: &[Value<B>]) {
+        self.h_struct_need_values(type_id, values.len() as u128);
+        for value in values {
+            self.value(value);
+        }
     }
 
-    fn r#enum<B: AsRef<[u8]> + ByteStorage>(&mut self, type_id: TypeId, var_id: VariantId) {
-        self.value(Value::Tuple(Box::new([
-            Value::<B>::TypeId(type_id),
-            Value::Uint(var_id),
-        ])));
-    }
-
-    fn choice<B: AsRef<[u8]> + ByteStorage>(&mut self, type_id: TypeId, var_id: VariantId, value: Value<B>) {
-        self.value(Value::Tuple(Box::new([
-            Value::TypeId(type_id),
-            Value::Uint(var_id),
-            value,
-        ])));
-    }
-
-    fn r#struct<B: AsRef<[u8]> + ByteStorage>(&mut self, type_id: TypeId, values: Box<[Value<B>]>) {
-        self.value(Value::Tuple(Box::new([
-            Value::TypeId(type_id),
-            Value::Tuple(values),
-        ])));
-    }
-
-    fn value<B: AsRef<[u8]> + ByteStorage>(&mut self, value: Value<B>) {
-        self.byte(value.as_tag() as u8);
+    fn value<B: AsRef<[u8]> + ByteStorage>(&mut self, value: &Value<B>) {
         match value {
-            Value::Uint(n) => self.uleb128(n),
-            Value::Int(n) => self.sleb128(n),
-            Value::Uints(uints) => self.uints(uints),
-            Value::Bytes(bytes) => self.bytes(bytes),
-            Value::String(usvs) => self.string(usvs),
-            Value::Tuple(values) => self.tuple(values),
-
-            Value::List(r#type, values) => self.list(r#type, values),
-            Value::Alias(type_id, value) => self.alias(type_id, *value),
-            Value::Enum(type_id, var_id) => self.r#enum::<B>(type_id, var_id),
-            Value::Choice(type_id, var_id, value) => self.choice(type_id, var_id, *value),
-            Value::Struct(type_id, values) => self.r#struct(type_id, values),
-            Value::Type(ty) => self.r#type::<B>(ty),
-            Value::TypeId(type_id) => self.type_id::<B>(type_id),
+            Value::Uint(n) => self.v_uint(*n),
+            Value::Int(n) => self.v_int(*n),
+            Value::Uints(uints) => self.v_uints(uints),
+            Value::Bytes(bytes) => self.v_bytes(bytes),
+            Value::String(chars) => self.v_string(chars),
+            Value::List(r#type, values) => self.v_list(r#type, values),
+            Value::Tuple(values) => self.v_tuple(values),
+            Value::Alias(type_id, value) => self.v_alias(type_id, value),
+            Value::Enum(type_id, var_id) => self.v_enum(type_id, *var_id),
+            Value::Choice(type_id, var_id, value) => self.v_choice(type_id, *var_id, value),
+            Value::Struct(type_id, values) => self.v_struct(type_id, values),
+            Value::Type(r#type) => self.v_type(r#type),
+            Value::TypeId(type_id) => self.v_type_id(type_id),
         }
     }
 }
 
 impl<B: AsRef<[u8]> + ByteStorage> Value<B> {
-    pub fn encode<O: Output>(self) -> O::Storage {
+    pub fn encode<O: Output>(&self) -> O::Storage {
         let mut writer = Writer::<O>::new();
         writer.value(self);
         writer.into_inner()
