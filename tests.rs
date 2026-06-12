660a81439f0d56c2912c83a253b384d02d5b1f1c
stackinspector <stackinspector@126.com>
2026-06-12 19:43:54 +0800

    test case

--- tests.rs
+++ tests.rs
@@ -96,4 +96,99 @@ fn cases() {
                             53 06 7468616e6b73
         "),
     );
+
+    const F64_BYTES: &[u8] = 50.0_f64.to_le_bytes().as_slice();
+
+    println!("{:?}", F64_BYTES);
+    case(
+        Value::Tuple(seq![
+            Value::Tuple(seq![]),
+            Value::Bool(false),
+            Value::Int(-7777777),
+            Value::Uint(1027),
+            Value::Alias(TypeId::Std(10), seq![], Box::new(Value::Bytes(F64_BYTES))),
+            Value::String(s("Berylsoft")),
+            Value::Bytes(b(b"(\x00)")),
+            Value::Option(Type::String, None),
+            Value::Option(Type::Bool, Some(Box::new(Value::Bool(true)))),
+            Value::Alias(TypeId::Anonymous/* third-party */, seq![], Box::new(Value::Bytes(b(b"\xff")))),
+            Value::Enum(TypeId::Std(0x5f50), 11),
+            Value::Choice(TypeId::Std(0x5f49), seq![], 5, Box::new(Value::Int(5))),
+            Value::Choice(TypeId::Std(0xfe00aa), seq![], 163, Box::new(Value::Uint(12))),
+            Value::Type(Type::List(Box::new(Type::List(Box::new(Type::Struct(TypeId::Anonymous, seq![])))))),
+            Value::TypeId(TypeId::Std(0xfedcba98765432)),
+            Value::Option(
+                Type::Tuple(seq![Type::Int, Type::Tuple(seq![]), Type::Bool]),
+                Some(Box::new(Value::Tuple(seq![Value::Int(9), Value::Tuple(seq![]), Value::Bool(true)])))
+            ),
+        ]),
+        expb!("
+        50 10
+            50 00
+            46 00
+            49 8fa4a57c
+            55 8308
+            41 03
+                44 02
+                    55 79
+                    55 0a
+                47 00
+                42 08 0000000000004940
+            53 09 426572796c736f6674
+            42 03 280029
+            4f 02
+                46 00
+                54 02
+                    55 73
+                    50 00
+            4f 02
+                46 01
+                46 01
+            41 03
+                44 02
+                    55 78
+                    50 00
+                47 00
+                42 01 ff
+            45 02
+                44 02
+                    55 79
+                    55 d0be01
+                55 0b
+            43 04
+                44 02
+                    55 79
+                    55 c9be01
+                47 00
+                55 05
+                49 05
+            43 04
+                44 02
+                    55 79
+                    55 aa81f807
+                47 00
+                55 a301
+                55 0c
+            54 02
+                55 6c
+                54 02
+                    55 6c
+                    54 02
+                        55 72
+                        50 02
+                            44 02
+                                55 78
+                                50 00
+                            47 00
+            44 02
+                55 79
+                55 b2a8d9c3a997b77f
+            4f 02
+                46 01
+                50 03
+                    49 09
+                    50 00
+                    46 01
+        ")
+    );
 }
